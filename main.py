f0b6189f4df95cc9fe4b60b7097ef12db74ad422
Gmin77 <gongminseung@gmail.com>
2024-04-01 16:46:00 +0900

    add

--- main.py
+++ main.py
@@ -1,39 +1,50 @@
 import streamlit as st
-from pyparsing import empty
 import datetime as dt
+import csv
+import pandas as pd
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="centered")
 
-LEFTGAP = 0.2
-RIGHTGAP = 0.2
+# CSV 파일 경로
+file_path = 'daily.csv'
 
-left, con1, right = st.columns([0.3, 0.7, 0.3])
-left, con2, right = st.columns([0.35, 0.7, 0.5])
-left, con3, right = st.columns([0.3, 0.7, 0.3])
+# 현재 날짜
+now = dt.datetime.now()
 
-def date():
-    st.subheader(':date:일정확인')
-    if st.button("일정추가", type = 'primary') :
-            d = st.text_input("일정 날자를 입력하세요!")
-            st.write('추가 일정 날짜:', d)
-            work = st.text_input('일정을 추가하세요!', placeholder='Input Daily Work!')
-            st.write('일정 추가:', work)
+def add_daily(write_day, add_time,add_work):
+    with open(file_path, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([write_day, add_time ,add_work])
 
-def add_daily() :
-    pass
+def day_list():
+    daily = []
+
+    # 7일간의 날짜 생성
+    for i in range(7):
+        day = now + dt.timedelta(days=i)
+        write_day = day.strftime('%y-%m-%d')
+        daily.append(write_day)
+
+    add_date = st.date_input(label = '원하시는 날짜를 선택해주세요!')
+    add_time = st.time_input('원하시는 시간을 선택해주세요!', value=None)
+    add_work = st.text_input(label = ':calendar: 일정을 추가해주세요!', placeholder='Ex) Study', max_chars=255)
+    add_button = st.button(label="일정추가", type='primary')
+    if add_button :
+        add_daily(add_date, add_time, add_work)
+        st.write('추가된 일정 :', add_work) 
+
+def db_frame() :
+    data = pd.read_csv('daily.csv', encoding='cp949')
+    data.columns =['Date', 'Time', 'Work']
+    frame = pd.DataFrame(data)
+    frame
+
 def main():
+    st.title(':balloon: 일정추가')
+    st.divider()
+    day_list()
+    db_frame()
 
-    with left : 
-        empty()
 
-    with con1 :
-        st.title(':balloon: Daily Table')
-        st.divider()
-   
-    with con2:
-        date()
-
-    with con3:
-      pass
-    
-main()
+if __name__ == "__main__":
+    main()
